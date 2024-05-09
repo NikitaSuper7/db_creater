da2ac293cc0e9484bf3884a9469418517e34823d
Nike_S7 <nike7cr77@ya.ru>
2024-05-09 19:04:07 +0300

    refactor employer

--- src/employers.py
+++ src/employers.py
@@ -19,7 +19,7 @@ class Employe:
         for employer in employers_hh.employers_data:
             cls.all_employers.append(cls(employer_id=employer['id'], name=employer['name'],
                                          link=employer['alternate_url'], vacancies=employer['vacancies_url']))
-# if __name__ == '__main__':
-#     Employe.employ_maker()
-#     for employ in Employe.all_employers[:4]:
-#         print(employ.employer_id, employ.name, employ.link, employ.vacancies, sep=' - ')
+if __name__ == '__main__':
+    Employe.employ_maker()
+    for employ in Employe.all_employers[:4]:
+        print(employ.employer_id, employ.name, employ.link, employ.vacancies, sep=' - ')
